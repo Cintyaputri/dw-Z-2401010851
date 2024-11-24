f6e5b9d02ed409018083d6a5c0a4e6f8bfd49e73
Cintya Putri <putriragep6@gmail.com>
2024-11-24 07:30:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ document.querySelector("#menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-//Klik di luar sidebar untuk menghilangkan nav
+// Toggle class active search
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//Klik di luar element
 const menu = document.querySelector("#menu");
+const search = document.querySelector("#search");
 
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
